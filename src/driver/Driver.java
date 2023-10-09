a9a31048392b25058907b54a08894d95fe5a6827
Drake <dferrel@emich.edu>
2023-10-09 18:19:20 -0400

    Added Hungry

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -5,5 +5,6 @@ public class Driver {
 		System.out.println("Our team is getting started.");
 		Glad.glad();
 		Sad.Sad();
+		Hungry.hungry();
 	}
 }
---
+++ src/driver/Hungry.java
@@ -0,0 +1,7 @@
+package driver;
+
+public class Hungry {
+	public static void hungry() {
+		System.out.println("We are the most sad maybe ever");
+	}
+}

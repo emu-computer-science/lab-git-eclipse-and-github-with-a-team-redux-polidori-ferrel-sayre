55274fb9a5e1daadde70feef0af52f3b0998a5f9
Drake <dferrel@emich.edu>
2023-10-09 18:09:49 -0400

    Driver

---
+++ src/driver/Driver.java
@@ -0,0 +1,7 @@
+package driver;
+
+public class Driver {
+	public static void main(String[] args) {
+		System.out.println("Our team is getting started.");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author emu
+ *
+ */
+module WhateverYouWant {
+}

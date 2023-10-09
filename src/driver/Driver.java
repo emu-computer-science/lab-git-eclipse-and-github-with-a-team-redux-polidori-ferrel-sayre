18b0300db1c71de34aeb579f0cbfa9a2790ea9a0
seanpolid <seanpolidori0@gmail.com>
2023-10-09 18:12:44 -0400

    Created Glad

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -3,5 +3,6 @@ package driver;
 public class Driver {
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started.");
+		Glad.glad();
 	}
 }
---
+++ src/driver/Glad.java
@@ -0,0 +1,9 @@
+package driver;
+
+public class Glad {
+
+	public static void glad() {
+		System.out.println("We are all sad.");
+	}
+	
+}
